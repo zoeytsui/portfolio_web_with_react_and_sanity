5c375a38552b93cde6ebb76e649ea36e846210b6
Zoey X <zoey.xu@aax.com>
2024-02-08 18:15:58 +0800

    feature: pinch zoom image

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useMemo, useCallback } from 'react';
+import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
+import QuickPinchZoom, { make3dTransformValue } from "react-quick-pinch-zoom";
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
@@ -23,6 +24,18 @@ const Work = () => {
     setImageData({url:img, alt:alt})
   },[])
 
+  const imgRef = useRef()
+  const onUpdate = useCallback(({ x, y, scale }) => {
+    const { current: img } = imgRef;
+
+    if (img) {
+      const value = make3dTransformValue({ x, y, scale });
+
+      img.style.setProperty("transform", value);
+    }
+  }, []);
+
+  // TODO: implement image pinch or zoom feature 
   const ImageModal = useMemo(()=>{
     document.body.style.overflow = showModal ? "hidden" : "auto"
     return showModal
@@ -30,13 +43,14 @@ const Work = () => {
           className='ImageModal'
           onClick={()=>setShowModal(false)} 
         >
-          <img src={imageData.url} alt={imageData.alt} />
+          <QuickPinchZoom onUpdate={onUpdate}>
+            <img ref={imgRef} src={imageData.url} alt={imageData.alt} />
+          </QuickPinchZoom>
         </div> 
       : null
-  },[showModal, imageData])
+  },[showModal, imageData,imgRef, onUpdate])
 
   useEffect(() => {
-    // TODO: implement image pinch or zoom feature 
     const query = '*[_type == "works"]';
 
     client.fetch(query).then((data) => {
@@ -44,6 +58,7 @@ const Work = () => {
       setFilterWork(data);
     });
   }, [showModal]);
+  
 
   const handleWorkFilter = (item) => {
     setActiveFilter(item);
@@ -69,7 +84,7 @@ const Work = () => {
             <div
               key={index}
               onClick={() => handleWorkFilter(item)}
-              className={`app__work-filter-item app__flex__works p-text ${activeFilter === item ? 'item-active' : ''}`}
+              className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
             >
               {item}
             </div>
@@ -83,20 +98,20 @@ const Work = () => {
       >
         {filterWork.sort((a, b) => new Date(b._updatedAt) - new Date(a._updatedAt)).map((work, index) => (
           <div className="app__work-item app__flex__works" key={index}>
-            <div className="app__work-img app__flex__works" onClick={()=>handleClickImg(urlFor(work.imgUrl), work.name)}>
+            <div className="app__work-img app__flex" onClick={()=>handleClickImg(urlFor(work.imgUrl), work.name)}>
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className="app__work-hover app__flex__works"
+                className="app__work-hover app__flex"
               >
                 {work.projectLink&&<a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
-                    className="app__flex__works"
+                    className="app__flex"
                   >
                     <AiFillEye />
                   </motion.div>
@@ -106,7 +121,7 @@ const Work = () => {
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
-                    className="app__flex__works"
+                    className="app__flex"
                   >
                     <AiFillGithub />
                   </motion.div>
@@ -114,11 +129,11 @@ const Work = () => {
               </motion.div>
             </div>
 
-            <div className="app__work-content app__flex__works">
+            <div className="app__work-content app__flex">
               <h4 className="bold-text">{work.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
 
-              <div className="app__work-tag app__flex__works">
+              <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
               </div>
             </div>
