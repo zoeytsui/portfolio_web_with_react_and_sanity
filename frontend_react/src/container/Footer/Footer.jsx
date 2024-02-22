05c9355f5c43544b84ec8d7d90a0fead9aa02599
Zoey Tsui <68736693+zoeytsui@users.noreply.github.com>
2024-02-22 18:19:25 +0800

    Update Footer.jsx

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -122,7 +122,7 @@ const Footer = () => {
       )}
 
       <div className="copyright">
-        <p className="p-text">@2021 Zoey Xu</p>
+        <p className="p-text">@2024 Zoey Xu</p>
         <p className="p-text">All rights reserved</p>
       </div>
     </>
